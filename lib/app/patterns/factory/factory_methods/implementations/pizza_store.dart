fa652b76bf75592d8807ca3c3b661417bbb3ba6c
celiorscosta <celiorscosta@hotmail.com>
2021-08-19 15:02:05 -0300

    refactoring pizza store.

--- lib/app/patterns/factory/factory_methods/implementations/pizza_store.dart
+++ lib/app/patterns/factory/factory_methods/implementations/pizza_store.dart
@@ -1,17 +1,14 @@
-import 'package:head_first_design_patterns_dart/app/patterns/factory/simple_factory/factory/simple_pizza_factory.dart';
 import 'package:head_first_design_patterns_dart/app/patterns/factory/simple_factory/interface/pizza.dart';
 
-class PizzaStore {
-  final SimplePizzaFactory _factory;
-
-  PizzaStore(this._factory);
-
+abstract class PizzaStore {
   Pizza OrderPizza(String type) {
-    var pizza = _factory.createPizza(type);
+    var pizza = createPizza(type);
     pizza.prepare();
     pizza.bake();
     pizza.cut();
     pizza.box();
     return pizza;
   }
+
+  Pizza createPizza(String type);
 }
